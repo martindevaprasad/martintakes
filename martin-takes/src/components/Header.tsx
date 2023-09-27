06cf090f034852890c87875c118c882bed56eac7
martindevaprasad <martindevaprasad46@gmail.com>
2023-09-27 17:33:18 +0530

    updatedresum

--- martin-takes/src/components/Header.tsx
+++ martin-takes/src/components/Header.tsx
@@ -2,7 +2,6 @@ import AppBar from "@mui/material/AppBar";
 
 import Toolbar from "@mui/material/Toolbar";
 
-import Typography from "@mui/material/Typography";
 
 import Container from "@mui/material/Container";
 
